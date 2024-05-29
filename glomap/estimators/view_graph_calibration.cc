5df6ef65802b28c97f71c0f3360959888a3ba26d
lpanaf <linpan@student.ethz.ch>
2024-05-29 17:50:01 +0200

    engine for view graph calibration

---
+++ glomap/estimators/view_graph_calibration.cc
@@ -0,0 +1,191 @@
+#include "view_graph_calibration.h"
+
+#include "glomap/estimators/cost_function.h"
+#include "glomap/math/two_view_geometry.h"
+
+namespace glomap {
+
+bool ViewGraphCalibrator::Solve(ViewGraph& view_graph,
+                        std::unordered_map<camera_t, Camera>& cameras,
+                        std::unordered_map<image_t, Image>& images) {
+    // Reset the problem
+    std::cout << "Start ViewGraphCalibrator" << std::endl;
+    Reset(cameras);
+    std::cout << "Reset done" << std::endl;
+   
+    // Set the solver options.
+    if (cameras.size() < 50)
+        options_.solver_options.linear_solver_type
+                    = ceres::DENSE_NORMAL_CHOLESKY;
+    else
+        options_.solver_options.linear_solver_type
+                    = ceres::SPARSE_NORMAL_CHOLESKY;
+
+    // Add the image pairs into the problem
+    AddImagePairsToProblem(view_graph, cameras, images);
+    std::cout << "AddImagePairsToProblem done" << std::endl;
+
+    // Set the cameras to be constant if they have prior intrinsics
+    size_t num_cameras = ParameterizeCameras(cameras);
+
+    std::cout << "ParameterizeCameras done" << std::endl;
+
+    if (num_cameras == 0) {
+        std::cout << "No cameras to optimize" << std::endl;
+        return true;
+    }
+
+    // Solve the problem
+    ceres::Solver::Summary summary;
+    options_.solver_options.minimizer_progress_to_stdout = options_.verbose;
+    ceres::Solve(options_.solver_options, problem_.get(), &summary);
+
+    // Print the summary only if verbose
+    if (options_.verbose)
+        std::cout << summary.FullReport() << std::endl;
+    
+    // Convert the results back to the camera
+    ConvertResults(cameras);
+
+    // Filter the image pairs
+    FilterImagePair(view_graph, images);
+
+    return summary.IsSolutionUsable();
+}
+
+void ViewGraphCalibrator::Reset(std::unordered_map<camera_t, Camera>& cameras) {
+    // Initialize the problem
+    focals_.clear();
+    for (auto &[camera_id, camera] : cameras) {
+        focals_[camera_id] = camera.Focal();
+    }
+
+    // Set up the problem
+    problem_.reset(new ceres::Problem);
+    options_.loss_function = new ceres::CauchyLoss(options_.thres_loss_function);
+}
+
+void ViewGraphCalibrator::AddImagePairsToProblem(ViewGraph& view_graph,
+                        std::unordered_map<camera_t, Camera>& cameras,
+                        std::unordered_map<image_t, Image>& images) {
+    for (auto &[image_pair_id, image_pair] : view_graph.image_pairs) {
+        if (image_pair.config != 2 && image_pair.config != 3) 
+            continue;
+        if (image_pair.is_valid == false)
+            continue;
+        
+        AddImagePair(image_pair, cameras, images);
+    }
+}
+    
+void ViewGraphCalibrator::AddImagePair(ImagePair& image_pair,
+                        std::unordered_map<camera_t, Camera>& cameras,
+                        std::unordered_map<image_t, Image>& images) {
+    // Collect the cost terms
+    image_t image_id1 = image_pair.image_id1;
+    image_t image_id2 = image_pair.image_id2;
+
+    camera_t camera_id1 = images.at(image_id1).camera_id;
+    camera_t camera_id2 = images.at(image_id2).camera_id;
+
+    ceres::CostFunction* cost_function;
+    if (camera_id1 == camera_id2) {
+        cost_function = FetzerFocalLengthSameCameraCost::Create(image_pair.F, cameras[camera_id1].PrincipalPoint());
+        problem_->AddResidualBlock(cost_function,
+                                options_.loss_function,
+                                &(focals_[camera_id1]));
+    } else {
+        cost_function = FetzerFocalLengthCost::Create(image_pair.F, cameras[camera_id1].PrincipalPoint(), cameras[camera_id2].PrincipalPoint());
+        problem_->AddResidualBlock(cost_function,
+                                options_.loss_function,
+                                &(focals_[camera_id1]),
+                                &(focals_[camera_id2]));
+    }
+}
+
+size_t ViewGraphCalibrator::ParameterizeCameras(std::unordered_map<camera_t, Camera>& cameras) {
+    size_t num_cameras = 0;
+    for (auto &[camera_id, camera] : cameras) {
+        if (!problem_->HasParameterBlock(&(focals_[camera_id]))) 
+            continue;
+
+        num_cameras++;
+        problem_->SetParameterLowerBound(&(focals_[camera_id]), 0, 1e-3);
+        if (camera.has_prior_focal_length) {
+            problem_->SetParameterBlockConstant(&(focals_[camera_id]));
+            num_cameras--;
+        }
+    }
+
+    return num_cameras;
+}
+
+void ViewGraphCalibrator::ConvertResults(std::unordered_map<camera_t, Camera>& cameras) {
+    size_t counter = 0;
+    for (auto &[camera_id, camera] : cameras) {
+        if (!problem_->HasParameterBlock(&(focals_[camera_id])))
+            continue;
+            
+        // if the estimated parameter is too crazy, reject it
+        if (focals_[camera_id] / camera.Focal() > options_.thres_higher_ratio
+        || focals_[camera_id] / camera.Focal() < options_.thres_lower_ratio) {
+            if (options_.verbose)
+                std::cout << "NOT ACCEPTED: Camera "<< camera_id
+                        << " focal: " << focals_[camera_id]
+                        << " original focal: " << camera.Focal()
+                        << std::endl;
+            counter++;
+            
+            continue;
+        }
+
+        // Marke that the camera has refined intrinsics
+        camera.has_refined_focal_length = true;
+
+        // Update the focal length
+        for (const size_t idx : camera.FocalLengthIdxs()) {
+            camera.params[idx] = focals_[camera_id];
+        }
+    }
+    std::cout << counter << " cameras are rejected in view graph calibration" << std::endl;
+}
+
+size_t ViewGraphCalibrator::FilterImagePair(ViewGraph& view_graph,
+                        std::unordered_map<image_t, Image>& images) {
+    ceres::Problem::EvaluateOptions EvalOpts;
+    EvalOpts.num_threads = 16;
+    EvalOpts.apply_loss_function = false;
+    std::vector<double> residuals;
+    problem_->Evaluate(EvalOpts, nullptr, &residuals, nullptr, nullptr);
+
+
+    // Dump the residuals into the original data structure
+    size_t counter = 0;
+    size_t invalid_counter = 0;
+
+    for (auto &[image_pair_id, image_pair] : view_graph.image_pairs) {
+        if (image_pair.config != 2 && image_pair.config != 3) 
+            continue;
+        if (image_pair.is_valid == false)
+            continue;
+
+        Eigen::Vector2d error = Eigen::Vector2d(residuals[counter], residuals[counter+1]);
+        
+        image_t image_id1 = image_pair.image_id1;
+        image_t image_id2 = image_pair.image_id2;
+
+        // Set the two view geometry to be invalid if the error is too high
+        if (error.norm() > options_.thres_two_view_error) {
+            invalid_counter++;
+            image_pair.is_valid = false;
+        }
+        counter += 2;
+    }
+
+    std::cout << "invalid / total number of two view geometry: " << invalid_counter << " / " << counter << std::endl;
+
+    return invalid_counter;
+    
+}
+
+} // glomap
---
+++ glomap/estimators/view_graph_calibration.h
@@ -0,0 +1,69 @@
+#pragma once
+#ifndef GLOMAP_ESTIMATOR_VIEW_GRAPH_CALIBRATION_H_
+#define GLOMAP_ESTIMATOR_VIEW_GRAPH_CALIBRATION_H_
+
+
+#include "glomap/estimators/optimization_base.h"
+#include "glomap/scene/types_sfm.h"
+
+namespace glomap {
+
+struct ViewGraphCalibratorOptions : public OptimizationBaseOptions {
+    // The minimal ratio of the estimated focal length against the prior focal length
+    double thres_lower_ratio = 0.1;
+    // The maximal ratio of the estimated focal length against the prior focal length
+    double thres_higher_ratio = 10;
+
+    // The threshold for the corresponding error in the problem for an image pair
+    double thres_two_view_error = 2.;
+
+    ViewGraphCalibratorOptions()
+        : OptimizationBaseOptions() {
+        thres_loss_function = 1e-2;
+    }
+};
+
+
+class ViewGraphCalibrator {
+public:
+    ViewGraphCalibrator(const ViewGraphCalibratorOptions& options) :
+                        options_(options) {
+    };
+
+    // Entry point for the calibration
+    bool Solve(ViewGraph& view_graph,
+                        std::unordered_map<camera_t, Camera>& cameras,
+                        std::unordered_map<image_t, Image>& images);
+
+private:
+    // Reset the problem
+    void Reset(std::unordered_map<camera_t, Camera>& cameras);
+
+    // Add the image pairs to the problem
+    void AddImagePairsToProblem(ViewGraph& view_graph,
+                        std::unordered_map<camera_t, Camera>& cameras,
+                        std::unordered_map<image_t, Image>& images);
+
+    // Add a single image pair to the problem
+    void AddImagePair(ImagePair& image_pair,
+                        std::unordered_map<camera_t, Camera>& cameras,
+                        std::unordered_map<image_t, Image>& images);
+
+    // Set the cameras to be constant if they have prior intrinsics
+    size_t ParameterizeCameras(std::unordered_map<camera_t, Camera>& cameras);
+
+    // Convert the results back to the camera
+    void ConvertResults(std::unordered_map<camera_t, Camera>& cameras);
+
+    // Filter the image pairs based on the calibration results
+    size_t FilterImagePair(ViewGraph& view_graph,
+                        std::unordered_map<image_t, Image>& images);
+
+    ViewGraphCalibratorOptions options_;
+    std::unique_ptr<ceres::Problem> problem_;
+    std::unordered_map<camera_t, double> focals_;
+
+};
+
+} // glomap
+#endif // GLOMAP_ESTIMATOR_VIEW_GRAPH_CALIBRATION_H_
