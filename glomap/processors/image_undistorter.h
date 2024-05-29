f6d6b86111af4883670b16339f149a980622d4bc
lpanaf <linpan@student.ethz.ch>
2024-05-29 17:58:52 +0200

    method for undistorting images

---
+++ glomap/processors/image_undistorter.h
@@ -0,0 +1,14 @@
+#ifndef GLOMAP_PROCESSOR_IMAGE_UNDISTORTER_H_
+#define GLOMAP_PROCESSOR_IMAGE_UNDISTORTER_H_
+
+#include <glomap/scene/types_sfm.h>
+
+namespace glomap {
+
+void UndistortImages(std::unordered_map<camera_t, Camera>& cameras,
+                        std::unordered_map<image_t, Image>& images,
+                        bool clean_points = true);
+
+}; // glomap
+
+#endif // GLOMAP_PROCESSOR_IMAGE_UNDISTORTER_H_
