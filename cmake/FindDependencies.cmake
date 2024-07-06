c76c09e884ec007a77730515d6a9435e5983b0c3
Johannes Schönberger <joschonb@microsoft.com>
2024-07-06 13:52:18 +0000

    Cmake cleanup

---
+++ cmake/FindDependencies.cmake
@@ -0,0 +1,30 @@
+include(FetchContent)
+FetchContent_Declare(PoseLib
+    GIT_REPOSITORY    https://github.com/PoseLib/PoseLib.git
+    GIT_TAG           b3691b791bcedccd5451621b2275a1df0d9dcdeb
+)
+message(STATUS "Configuring PoseLib...")
+FetchContent_MakeAvailable(PoseLib)
+message(STATUS "Configuring PoseLib... done")
+
+FetchContent_Declare(COLMAP
+    GIT_REPOSITORY    https://github.com/colmap/colmap.git
+    GIT_TAG           cfdf8da62c9af2269fd965efb33ce8147e8d5d13
+)
+message(STATUS "Configuring COLMAP...")
+set(UNINSTALL_ENABLED OFF CACHE INTERNAL "")
+FetchContent_MakeAvailable(COLMAP)
+message(STATUS "Configuring COLMAP... done")
+
+find_package(Eigen3 3.4 REQUIRED)
+find_package(Ceres 2.2 REQUIRED COMPONENTS SuiteSparse)
+
+if(TESTS_ENABLED)
+    message(STATUS "Enabling tests")
+    find_package(GTest REQUIRED)
+endif()
+
+if (OPENMP_ENABLED)
+    message(STATUS "Enabling OpenMP")
+    find_package(OpenMP REQUIRED)
+endif()
