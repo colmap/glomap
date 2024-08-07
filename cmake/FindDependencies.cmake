6523a4ba9c4505d04a42cabbf9928309a144526a
Johannes Schönberger <joschonb@microsoft.com>
2024-08-07 19:16:30 +0000

    d

--- cmake/FindDependencies.cmake
+++ cmake/FindDependencies.cmake
@@ -1,5 +1,31 @@
 set(CMAKE_MODULE_PATH "${CMAKE_SOURCE_DIR}/cmake")
 
+find_package(Eigen3 3.4 REQUIRED)
+find_package(SuiteSparse COMPONENTS CHOLMOD REQUIRED)
+find_package(Ceres REQUIRED COMPONENTS SuiteSparse)
+find_package(Boost REQUIRED)
+
+if(IS_MSVC)
+    list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_SOURCE_DIR}")
+
+    find_package(Glog REQUIRED)
+    if(DEFINED glog_VERSION_MAJOR)
+        # Older versions of glog don't export version variables.
+        add_definitions("-DGLOG_VERSION_MAJOR=${glog_VERSION_MAJOR}")
+        add_definitions("-DGLOG_VERSION_MINOR=${glog_VERSION_MINOR}")
+    endif()
+endif()
+
+if(TESTS_ENABLED)
+    message(STATUS "Enabling tests")
+    find_package(GTest REQUIRED)
+endif()
+
+if (OPENMP_ENABLED)
+    message(STATUS "Enabling OpenMP")
+    find_package(OpenMP REQUIRED)
+endif()
+
 include(FetchContent)
 FetchContent_Declare(PoseLib
     GIT_REPOSITORY    https://github.com/PoseLib/PoseLib.git
@@ -27,29 +53,3 @@ else()
     find_package(COLMAP REQUIRED)
 endif()
 message(STATUS "Configuring COLMAP... done")
-
-find_package(Eigen3 3.4 REQUIRED)
-find_package(Ceres REQUIRED COMPONENTS SuiteSparse)
-find_package(Boost REQUIRED)
-find_package(SuiteSparse COMPONENTS CHOLMOD REQUIRED)
-
-if(IS_MSVC)
-    list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_SOURCE_DIR}")
-
-    find_package(Glog REQUIRED)
-    if(DEFINED glog_VERSION_MAJOR)
-        # Older versions of glog don't export version variables.
-        add_definitions("-DGLOG_VERSION_MAJOR=${glog_VERSION_MAJOR}")
-        add_definitions("-DGLOG_VERSION_MINOR=${glog_VERSION_MINOR}")
-    endif()
-endif()
-
-if(TESTS_ENABLED)
-    message(STATUS "Enabling tests")
-    find_package(GTest REQUIRED)
-endif()
-
-if (OPENMP_ENABLED)
-    message(STATUS "Enabling OpenMP")
-    find_package(OpenMP REQUIRED)
-endif()
