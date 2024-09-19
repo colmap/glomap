4c1828dbf515e8c20e824a978d23dbd692056c04
Johannes Schönberger <jsch@demuc.de>
2024-09-19 09:39:04 +0200

    Use colmap thread pool to simplify mac installation

--- glomap/estimators/relpose_estimation.cc
+++ glomap/estimators/relpose_estimation.cc
@@ -1,5 +1,7 @@
 #include "glomap/estimators/relpose_estimation.h"
 
+#include <colmap/util/threading.h>
+
 #include <PoseLib/robust.h>
 
 namespace glomap {
@@ -14,14 +16,13 @@ void EstimateRelativePoses(ViewGraph& view_graph,
     valid_pair_ids.push_back(image_pair_id);
   }
 
-  // Define outside loop to reuse memory and avoid reallocation.
-  std::vector<Eigen::Vector2d> points2D_1, points2D_2;
-  std::vector<char> inliers;
-
   const int64_t num_image_pairs = valid_pair_ids.size();
   const int64_t kNumChunks = 10;
   const int64_t interval =
       std::ceil(static_cast<double>(num_image_pairs) / kNumChunks);
+
+  colmap::ThreadPool thread_pool(colmap::ThreadPool::kMaxNumThreads);
+
   LOG(INFO) << "Estimating relative pose for " << num_image_pairs << " pairs";
   for (int64_t chunk_id = 0; chunk_id < kNumChunks; chunk_id++) {
     std::cout << "\r Estimating relative pose: " << chunk_id * kNumChunks << "%"
@@ -30,47 +31,55 @@ void EstimateRelativePoses(ViewGraph& view_graph,
     const int64_t end =
         std::min<int64_t>((chunk_id + 1) * interval, num_image_pairs);
 
-#pragma omp parallel for schedule(dynamic) private( \
-    points2D_1, points2D_2, inliers)
     for (int64_t pair_idx = start; pair_idx < end; pair_idx++) {
-      ImagePair& image_pair = view_graph.image_pairs[valid_pair_ids[pair_idx]];
-      const Image& image1 = images[image_pair.image_id1];
-      const Image& image2 = images[image_pair.image_id2];
-      const Eigen::MatrixXi& matches = image_pair.matches;
+      thread_pool.AddTask([&, pair_idx]() {
+        // Define as thread-local to reuse memory allocation in different tasks.
+        thread_local std::vector<Eigen::Vector2d> points2D_1;
+        thread_local std::vector<Eigen::Vector2d> points2D_2;
+        thread_local std::vector<char> inliers;
 
-      // Collect the original 2D points
-      points2D_1.clear();
-      points2D_2.clear();
-      for (size_t idx = 0; idx < matches.rows(); idx++) {
-        points2D_1.push_back(image1.features[matches(idx, 0)]);
-        points2D_2.push_back(image2.features[matches(idx, 1)]);
-      }
+        ImagePair& image_pair =
+            view_graph.image_pairs[valid_pair_ids[pair_idx]];
+        const Image& image1 = images[image_pair.image_id1];
+        const Image& image2 = images[image_pair.image_id2];
+        const Eigen::MatrixXi& matches = image_pair.matches;
 
-      inliers.clear();
-      poselib::CameraPose pose_rel_calc;
-      try {
-        poselib::estimate_relative_pose(
-            points2D_1,
-            points2D_2,
-            ColmapCameraToPoseLibCamera(cameras[image1.camera_id]),
-            ColmapCameraToPoseLibCamera(cameras[image2.camera_id]),
-            options.ransac_options,
-            options.bundle_options,
-            &pose_rel_calc,
-            &inliers);
-      } catch (const std::exception& e) {
-        LOG(ERROR) << "Error in relative pose estimation: " << e.what();
-        image_pair.is_valid = false;
-        continue;
-      }
+        // Collect the original 2D points
+        points2D_1.clear();
+        points2D_2.clear();
+        for (size_t idx = 0; idx < matches.rows(); idx++) {
+          points2D_1.push_back(image1.features[matches(idx, 0)]);
+          points2D_2.push_back(image2.features[matches(idx, 1)]);
+        }
 
-      // Convert the relative pose to the glomap format
-      for (int i = 0; i < 4; i++) {
-        image_pair.cam2_from_cam1.rotation.coeffs()[i] =
-            pose_rel_calc.q[(i + 1) % 4];
-      }
-      image_pair.cam2_from_cam1.translation = pose_rel_calc.t;
+        inliers.clear();
+        poselib::CameraPose pose_rel_calc;
+        try {
+          poselib::estimate_relative_pose(
+              points2D_1,
+              points2D_2,
+              ColmapCameraToPoseLibCamera(cameras[image1.camera_id]),
+              ColmapCameraToPoseLibCamera(cameras[image2.camera_id]),
+              options.ransac_options,
+              options.bundle_options,
+              &pose_rel_calc,
+              &inliers);
+        } catch (const std::exception& e) {
+          LOG(ERROR) << "Error in relative pose estimation: " << e.what();
+          image_pair.is_valid = false;
+          return;
+        }
+
+        // Convert the relative pose to the glomap format
+        for (int i = 0; i < 4; i++) {
+          image_pair.cam2_from_cam1.rotation.coeffs()[i] =
+              pose_rel_calc.q[(i + 1) % 4];
+        }
+        image_pair.cam2_from_cam1.translation = pose_rel_calc.t;
+      });
     }
+
+    thread_pool.Wait();
   }
 
   std::cout << "\r Estimating relative pose: 100%" << std::endl;
